4278249f84c07a17d9a1d4f450e0c8c8b7dc1819
prayz212 <49938194+prayz212@users.noreply.github.com>
2026-06-27 21:21:01 +0700

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  identityServerOrigin: 'http://localhost:15201',
+  identityServerOrigin: 'https://localhost:15200',
   oidcClientId: 'simplify-yours-web',
   oidcRedirectUri: 'http://localhost:4200/auth/callback',
   oidcScopes: 'openid profile email offline_access',
